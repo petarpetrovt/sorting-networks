21e13a6107595eecb32ed81a97d0f67da248a24d
Petar Petrov <ppetrov@david.bg>
2020-09-19 21:36:10 +0300

    Update editorconfig

--- test/SortingNetworks.Tests/SNTests.cs
+++ test/SortingNetworks.Tests/SNTests.cs
@@ -1,8 +1,8 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-
-namespace SortingNetworks.Tests
+﻿namespace SortingNetworks.Tests
 {
+	using System;
+	using Microsoft.VisualStudio.TestTools.UnitTesting;
+
 	[TestClass]
 	public class SNTests : SNTestsBase
 	{
