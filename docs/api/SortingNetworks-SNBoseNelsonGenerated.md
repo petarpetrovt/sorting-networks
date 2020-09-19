de6800e71ab3bec68b2a0fb6c1ebac715ce56764
Petar Petrov <ppetrov@david.bg>
2020-09-19 17:07:47 +0300

    Add descending overloads to generated SN

--- docs/api/SortingNetworks-SNBoseNelsonGenerated.md
+++ docs/api/SortingNetworks-SNBoseNelsonGenerated.md
@@ -8,6 +8,10 @@ public class SNBoseNelsonGenerated : SNBase
 Inheritance [System.Object](https://docs.microsoft.com/en-us/dotnet/api/System.Object 'System.Object') &#129106; [SNBase](./SortingNetworks-SNBase.md 'SortingNetworks.SNBase') &#129106; SNBoseNelsonGenerated  
 ### Methods
 - [Sort2Ascending&lt;T&gt;(T)](./SortingNetworks-SNBoseNelsonGenerated-Sort2Ascending-T-(T).md 'SortingNetworks.SNBoseNelsonGenerated.Sort2Ascending&lt;T&gt;(T)')
+- [Sort2Descending&lt;T&gt;(T)](./SortingNetworks-SNBoseNelsonGenerated-Sort2Descending-T-(T).md 'SortingNetworks.SNBoseNelsonGenerated.Sort2Descending&lt;T&gt;(T)')
 - [Sort3Ascending&lt;T&gt;(T)](./SortingNetworks-SNBoseNelsonGenerated-Sort3Ascending-T-(T).md 'SortingNetworks.SNBoseNelsonGenerated.Sort3Ascending&lt;T&gt;(T)')
+- [Sort3Descending&lt;T&gt;(T)](./SortingNetworks-SNBoseNelsonGenerated-Sort3Descending-T-(T).md 'SortingNetworks.SNBoseNelsonGenerated.Sort3Descending&lt;T&gt;(T)')
 - [Sort4Ascending&lt;T&gt;(T)](./SortingNetworks-SNBoseNelsonGenerated-Sort4Ascending-T-(T).md 'SortingNetworks.SNBoseNelsonGenerated.Sort4Ascending&lt;T&gt;(T)')
+- [Sort4Descending&lt;T&gt;(T)](./SortingNetworks-SNBoseNelsonGenerated-Sort4Descending-T-(T).md 'SortingNetworks.SNBoseNelsonGenerated.Sort4Descending&lt;T&gt;(T)')
 - [Sort5Ascending&lt;T&gt;(T)](./SortingNetworks-SNBoseNelsonGenerated-Sort5Ascending-T-(T).md 'SortingNetworks.SNBoseNelsonGenerated.Sort5Ascending&lt;T&gt;(T)')
+- [Sort5Descending&lt;T&gt;(T)](./SortingNetworks-SNBoseNelsonGenerated-Sort5Descending-T-(T).md 'SortingNetworks.SNBoseNelsonGenerated.Sort5Descending&lt;T&gt;(T)')
--- src/SortingNetworks/SNBoseNelsonGenerated.Comparable.cs
+++ src/SortingNetworks/SNBoseNelsonGenerated.Comparable.cs
@@ -23,6 +23,19 @@
 		}
 
 		/// <summary>
+		/// Sorts a collection of 2 elements in descending order using the <see cref="IComparable{T}"/> generic
+		/// interface implementation of each element.
+		/// </summary>
+		/// <typeparam name="T">The type of the elements of the collection.</typeparam>
+		/// <param name="p0">the reference to the first element of the collection</param>
+		public static void Sort2Descending<T>(ref T p0)
+			where T : IComparable<T>
+		{
+			ref var p1 = ref Unsafe.Add(ref p0, 1);
+
+			SwapIfLesser(ref p0, ref p1);
+		}
+		/// <summary>
 		/// Sorts a collection of 3 elements in ascending order using the <see cref="IComparable{T}"/> generic
 		/// interface implementation of each element.
 		/// </summary>
@@ -39,6 +52,22 @@
 			SwapIfGreater(ref p0, ref p1);
 		}
 
+		/// <summary>
+		/// Sorts a collection of 3 elements in descending order using the <see cref="IComparable{T}"/> generic
+		/// interface implementation of each element.
+		/// </summary>
+		/// <typeparam name="T">The type of the elements of the collection.</typeparam>
+		/// <param name="p0">the reference to the first element of the collection</param>
+		public static void Sort3Descending<T>(ref T p0)
+			where T : IComparable<T>
+		{
+			ref var p1 = ref Unsafe.Add(ref p0, 1);
+			ref var p2 = ref Unsafe.Add(ref p1, 2);
+
+			SwapIfLesser(ref p1, ref p2);
+			SwapIfLesser(ref p0, ref p2);
+			SwapIfLesser(ref p0, ref p1);
+		}
 		/// <summary>
 		/// Sorts a collection of 4 elements in ascending order using the <see cref="IComparable{T}"/> generic
 		/// interface implementation of each element.
@@ -59,6 +88,25 @@
 			SwapIfGreater(ref p1, ref p2);
 		}
 
+		/// <summary>
+		/// Sorts a collection of 4 elements in descending order using the <see cref="IComparable{T}"/> generic
+		/// interface implementation of each element.
+		/// </summary>
+		/// <typeparam name="T">The type of the elements of the collection.</typeparam>
+		/// <param name="p0">the reference to the first element of the collection</param>
+		public static void Sort4Descending<T>(ref T p0)
+			where T : IComparable<T>
+		{
+			ref var p1 = ref Unsafe.Add(ref p0, 1);
+			ref var p2 = ref Unsafe.Add(ref p1, 2);
+			ref var p3 = ref Unsafe.Add(ref p2, 3);
+
+			SwapIfLesser(ref p0, ref p1);
+			SwapIfLesser(ref p2, ref p3);
+			SwapIfLesser(ref p0, ref p2);
+			SwapIfLesser(ref p1, ref p3);
+			SwapIfLesser(ref p1, ref p2);
+		}
 		/// <summary>
 		/// Sorts a collection of 5 elements in ascending order using the <see cref="IComparable{T}"/> generic
 		/// interface implementation of each element.
@@ -84,5 +132,29 @@
 			SwapIfGreater(ref p1, ref p2);
 		}
 
+		/// <summary>
+		/// Sorts a collection of 5 elements in descending order using the <see cref="IComparable{T}"/> generic
+		/// interface implementation of each element.
+		/// </summary>
+		/// <typeparam name="T">The type of the elements of the collection.</typeparam>
+		/// <param name="p0">the reference to the first element of the collection</param>
+		public static void Sort5Descending<T>(ref T p0)
+			where T : IComparable<T>
+		{
+			ref var p1 = ref Unsafe.Add(ref p0, 1);
+			ref var p2 = ref Unsafe.Add(ref p1, 2);
+			ref var p3 = ref Unsafe.Add(ref p2, 3);
+			ref var p4 = ref Unsafe.Add(ref p3, 4);
+
+			SwapIfLesser(ref p0, ref p1);
+			SwapIfLesser(ref p3, ref p4);
+			SwapIfLesser(ref p2, ref p4);
+			SwapIfLesser(ref p2, ref p3);
+			SwapIfLesser(ref p0, ref p3);
+			SwapIfLesser(ref p0, ref p2);
+			SwapIfLesser(ref p1, ref p4);
+			SwapIfLesser(ref p1, ref p3);
+			SwapIfLesser(ref p1, ref p2);
+		}
 	}
 }
