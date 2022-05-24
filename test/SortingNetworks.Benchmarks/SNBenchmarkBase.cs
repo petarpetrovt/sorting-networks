a6a8931482226a1bb77220581487fcf80e35c012
Petar Petrov <ppetrov@david.bg>
2022-05-24 18:27:29 +0300

    Remove corert benchmark runtime moniker

--- test/SortingNetworks.Benchmarks/SNBenchmarkBase.cs
+++ test/SortingNetworks.Benchmarks/SNBenchmarkBase.cs
@@ -3,9 +3,8 @@
 using BenchmarkDotNet.Attributes;
 using BenchmarkDotNet.Jobs;
 
-[SimpleJob(RuntimeMoniker.Net50, baseline: true)]
+[SimpleJob(RuntimeMoniker.Net50)]
 [SimpleJob(RuntimeMoniker.Net60)]
-[SimpleJob(RuntimeMoniker.CoreRt60)]
 [MemoryDiagnoser]
 public abstract class SNBenchmarkBase
 {
